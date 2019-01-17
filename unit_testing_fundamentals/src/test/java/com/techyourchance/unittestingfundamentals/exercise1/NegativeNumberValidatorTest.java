d3a3e3161a298a0c86e722b85ccebf6792ed5980
Belema Ogan <Oganbelema@gmail.com>
2019-01-17 19:00:48 +0100

    wrote a test for the NegativeNumberValidator class to ensure that the isNegative method fulfilled the business requirement which is to return true when a number less than 0 is passed to it. The test passed for negative and positive numbers but failed when I passed zero. I had to modify the code to check for only less than zero and not less or equal to zero.

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,35 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
 public class NegativeNumberValidatorTest {
 
+    NegativeNumberValidator SUT;
+
+    @Before
+    public void setup(){
+        SUT = new NegativeNumberValidator();
+    }
+
+    @Test
+    public void negativeNumberTest(){
+        boolean response = SUT.isNegative(-1);
+        Assert.assertThat(response, is(true));
+    }
+
+    @Test
+    public void whenNumberIsZeroTest(){
+        boolean response = SUT.isNegative(0);
+        Assert.assertThat(response, is(false));
+    }
+
+    @Test
+    public void positiveNumberTest(){
+        boolean response = SUT.isNegative(1);
+        Assert.assertThat(response, is(false));
+    }
 }
