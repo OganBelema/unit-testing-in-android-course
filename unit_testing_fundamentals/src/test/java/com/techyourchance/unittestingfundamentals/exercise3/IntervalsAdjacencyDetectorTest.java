85b5ebed130955bf83c2e1eeac66e770b578b7e3
Belema Ogan <Oganbelema@gmail.com>
2019-01-21 15:36:04 +0100

    wrote tests for the IntervalsAdjacencyDetector class to ensure that the isAdjacent method fulfilled the business requirement which is to return true when a interval2 starts the same time interval1 ends or interval1  starts the same time interval2 ends.  The test failed when interval1 started the same time interval2 ended and  I had to fix the logic error in the method.

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise3/IntervalsAdjacencyDetectorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise3/IntervalsAdjacencyDetectorTest.java
@@ -1,5 +1,76 @@
 package com.techyourchance.unittestingfundamentals.exercise3;
 
+import com.techyourchance.unittestingfundamentals.example3.Interval;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+
 public class IntervalsAdjacencyDetectorTest {
 
+    IntervalsAdjacencyDetector SUT;
+
+    @Before
+    public void setUp() throws Exception {
+        SUT = new IntervalsAdjacencyDetector();
+    }
+
+
+    @Test
+    public void isAdjacent_Interval1BeforeAndAdjacent_returnTrue() throws Exception {
+        Interval interval1 = new Interval(-3, 5);
+        Interval interval2 = new Interval(5, 9);
+        boolean result = SUT.isAdjacent(interval1, interval2);
+        Assert.assertThat(result, is(true));
+    }
+
+    @Test
+    public void isAdjacent_Interval1AfterAndAdjacent_returnTrue() throws Exception {
+        Interval interval1 = new Interval(7, 10);
+        Interval interval2 = new Interval(3, 7);
+        boolean result = SUT.isAdjacent(interval1, interval2);
+        Assert.assertThat(result, is(true));
+    }
+
+    @Test
+    public void isAdjacent_Interval1BeforeInterval2_returnFalse() throws Exception {
+        Interval interval1 = new Interval(-1, 4);
+        Interval interval2 = new Interval(6, 9);
+        boolean result = SUT.isAdjacent(interval1, interval2);
+        Assert.assertThat(result, is(false));
+    }
+
+    @Test
+    public void isAdjacent_Interval1AfterInterval2_returnFalse() throws Exception {
+        Interval interval1 = new Interval(6, 10);
+        Interval interval2 = new Interval(3, 5);
+        boolean result = SUT.isAdjacent(interval1, interval2);
+        Assert.assertThat(result, is(false));
+    }
+
+    @Test
+    public void isAdjacent_Interval1OverlapsInterval2OnStart_returnFalse() throws Exception {
+        Interval interval1 = new Interval(2, 10);
+        Interval interval2 = new Interval(5, 10);
+        boolean result = SUT.isAdjacent(interval1, interval2);
+        Assert.assertThat(result, is(false));
+    }
+
+    @Test
+    public void isAdjacent_Interval1OverlapsInterval2OnEnd_returnFalse() throws Exception {
+        Interval interval1 = new Interval(3, 7);
+        Interval interval2 = new Interval(3, 5);
+        boolean result = SUT.isAdjacent(interval1, interval2);
+        Assert.assertThat(result, is(false));
+    }
+
+    @Test
+    public void isAdjacent_Interval1ContainsInterval2_returnFalse() throws Exception {
+        Interval interval1 = new Interval(2, 8);
+        Interval interval2 = new Interval(4, 7);
+        boolean result = SUT.isAdjacent(interval1, interval2);
+        Assert.assertThat(result, is(false));
+    }
 }
